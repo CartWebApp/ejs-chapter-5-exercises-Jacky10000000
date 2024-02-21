665331392e87f185b8f6a5758f8330619775b2ca
Jacky10000000 <Leonardo.Castillo23@student.cart.org>
2024-02-20 19:01:31 -0800

    I killed the bird and climbed the tree and ended up finding a Gold Ore.

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,4 +1,9 @@
 // Your code here.
+function loop(value, test, update, body) {
+    for (let i = value; test(i); i = update(i)) {
+      body(i)
+    }
+  }
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
--- 3-everything.js
+++ 3-everything.js
@@ -1,6 +1,23 @@
 function every(array, test) {
-    // Your code here.
+  for (let element of array) {
+    if (test(element) === false) {
+      return false;
+    }
   }
+  return true;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+// every using array.some
+function every(array, test) {
+  return array.some(test);
+}
   
   console.log(every([1, 3, 5], n => n < 10));
   // → true
